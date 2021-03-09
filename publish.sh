d39a76039aa0995c15083bb7df9ad5bb78ae234c
Khalid Saifullah <khalid@outlook.com.au>
2021-03-10 08:33:37 +1100

    Release 0.0.6

--- publish.sh
+++ publish.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 message=$(git log -1 --pretty=%B)
-echo $message
-
 version=$(node ./version.js $message)
-echo $version
 
-if [[ -z ${version} ]]; then
+if [[ "$version" == "null" ]]; then
   echo "No new version to publish."
   exit 0
 fi
 
-# npm config set @brainstationau:registry https://registry.npmjs.org/
-# npm config set //registry.npmjs.org/:_authToken ${{ secrets.NPM_AUTH_TOKEN }}
-# npm publish
-# npm config delete //registry.npmjs.org/:_authToken
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+tag=$(npm version $version)
+git tag -a $tag -m "Publishing new version to NPM"
+
+npm config set @brainstationau:registry https://registry.npmjs.org/
+npm config set //registry.npmjs.org/:_authToken ${{ secrets.NPM_AUTH_TOKEN }}
+npm publish
+npm config delete //registry.npmjs.org/:_authToken
