8968b278358ef0a7553599948894ffadc993c9ea
Khalid Saifullah <khalid@outlook.com.au>
2021-05-09 08:19:47 +1000

    Add single select

---
+++ src/SingleSelect/index.tsx
@@ -0,0 +1,70 @@
+import {
+  FormControl,
+  FormControlProps,
+  FormHelperText,
+  InputLabel,
+  NativeSelect,
+  NativeSelectProps,
+} from '@material-ui/core';
+import { NativeSelectInputProps } from '@material-ui/core/NativeSelect/NativeSelectInput';
+import { useField } from 'formik';
+import React, { FunctionComponent } from 'react';
+
+export interface SelectOption {
+  label: string;
+  value: string;
+}
+
+export interface SingleSelectProps
+  extends Omit<FormControlProps, 'children' | 'error'> {
+  helperText?: string;
+  id?: string;
+  label: string;
+  name: string;
+  options: SelectOption[];
+  selectProps?: Omit<
+    NativeSelectProps,
+    'children' | 'inputProps' | 'onChange' | 'value' | 'variant'
+  > & {
+    inputProps?: Omit<NativeSelectInputProps, 'name' | 'id'>;
+  };
+}
+
+export const SingleSelect: FunctionComponent<SingleSelectProps> = ({
+  helperText,
+  id,
+  label,
+  name,
+  options,
+  selectProps,
+  ...formControlProps
+}) => {
+  const [field, meta] = useField<string>(name);
+  const formError = meta.touched && !!meta.error;
+  const elementId = id || `single-select-${Math.ceil(Math.random() * 1000)}`;
+  const { inputProps, ...nativeSelectProps } = selectProps || {};
+
+  return (
+    <FormControl {...formControlProps} error={formError}>
+      <InputLabel htmlFor={elementId}>{label}</InputLabel>
+      <NativeSelect
+        {...nativeSelectProps}
+        value={field.value}
+        onChange={field.onChange}
+        inputProps={{
+          ...inputProps,
+          id: elementId,
+          name,
+        }}
+      >
+        <option aria-label="None" value="" />
+        {options.map((o) => (
+          <option key={o.label} value={o.value}>
+            {o.label}
+          </option>
+        ))}
+      </NativeSelect>
+      <FormHelperText>{formError ? meta.error : helperText}</FormHelperText>
+    </FormControl>
+  );
+};
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { Checkbox, CheckboxProps } from './Checkbox';
 export { Checkboxes, CheckboxesProps, CheckboxOption } from './Checkboxes';
 export { NativeSelect, NativeSelectOption, NativeSelectProps } from './NativeSelect';
 export { RadioGroup, RadioGroupProps, RadioOption } from './RadioGroup';
+export { SelectOption, SingleSelect, SingleSelectProps } from './SingleSelect';
 export { TextField, TextFieldProps } from './TextField';
