3fe520953c1830826647bf694c801e3ccf41f58c
Khalid Saifullah <khalid@outlook.com.au>
2021-05-09 08:57:27 +1000

    add Date Picker

---
+++ src/DatePicker/index.tsx
@@ -0,0 +1,44 @@
+import {
+  DatePicker as MuiDatePicker,
+  DatePickerProps as MuiDatePickerProps,
+} from '@material-ui/pickers';
+import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
+import { useField } from 'formik';
+import moment from 'moment';
+import React, { FunctionComponent } from 'react';
+
+export interface DatePickerProps
+  extends Omit<
+    MuiDatePickerProps,
+    'error' | 'format' | 'name' | 'onBlur' | 'onChange' | 'onError' | 'value'
+  > {
+  name: string;
+  format: string;
+}
+
+export const DatePicker: FunctionComponent<DatePickerProps> = ({
+  name,
+  format,
+  helperText,
+  ...otherProps
+}) => {
+  const [field, meta, helpers] = useField<string>(name);
+  const initialValue = field.value ? moment(field.value, format) : null;
+  const datepickerOnChange = (date: MaterialUiPickersDate) => {
+    const selectedDate = date ? date.format(format) : '';
+    helpers.setValue(selectedDate);
+  };
+  const formError = meta.touched && !!meta.error;
+
+  return (
+    <MuiDatePicker
+      value={initialValue}
+      onChange={datepickerOnChange}
+      format={format}
+      name={name}
+      helperText={formError ? meta.error : helperText}
+      error={formError}
+      {...otherProps}
+    />
+  );
+};
